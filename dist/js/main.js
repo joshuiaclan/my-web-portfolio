3450010f1ada2d9c45e9d2b256c80ffd533a8193
= <=>
2019-09-15 01:17:52 +0800

    Add menu functionality with animation

---
+++ dist/js/main.js
@@ -0,0 +1,33 @@
+// Select DOM Objects
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBrand = document.querySelector(".menu-brand");
+const navItems  = document.querySelectorAll(".menu-item");
+
+// Set initial state of the menu;
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+// functions
+function toggleMenu(){
+  if(!showMenu){
+    menuBtn.classList.add("close");  
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBrand.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+    showMenu = true;
+  // Set menu state
+  } else {
+    menuBtn.classList.remove("close");  
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBrand.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+    showMenu = false;
+  }
+  
+}
