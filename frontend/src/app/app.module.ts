e02f7bbecf61e3d932c41878e7f063aa0837c9d1
jmaclan <josh.aclan@gmail.com>
2021-01-01 01:38:24 +0800

    Add navbar components and other component templates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
+import { ContactInfoComponent } from './components/contact-info/contact-info.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    WorkExperienceComponent,
+    ContactInfoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
