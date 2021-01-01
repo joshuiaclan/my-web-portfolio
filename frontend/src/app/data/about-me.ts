2ca2fa69f68ffd0273e26b6248920b160a4632c8
jmaclan <josh.aclan@gmail.com>
2021-01-01 17:31:00 +0800

    Add data and update hero component

---
+++ frontend/src/app/data/about-me.ts
@@ -0,0 +1,7 @@
+export let aboutMe = {
+        name: "Joshua Aclan",
+        birthday: "12-03-1998",
+        intro: "Are you lost? I hope you're not, my name is on the left side of your screen, I am a fullstack developer by day and an ultimate frisbee player by sunday. I do web programming as good as I throw discs, if you dont know who good I am just believe me.... I am. If you're looking to hire me, heres a short intro... I am a goal driven person, who is bubbly and very optimistic. I can adapt easily to my surroundings, multi-task, and work under pressure while smiling. A fast learner and enthusiastic, I am mentally and physically fit to perform my job. If thats not enough feel free to click that contact button you see on the top right of the screen.",
+        jobPosition: "Fullstack Developer",
+    }
+;
