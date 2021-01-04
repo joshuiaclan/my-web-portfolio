a30c6dd10bb050da30f13208294121745f73ebe8
jmaclan <josh.aclan@gmail.com>
2021-01-04 09:11:47 +0800

    Make footer

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,15 +1,71 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
-
+import {
+	faInstagram,
+	faTwitter,
+	faGithub,
+	faFacebook,
+	faDev,
+	faLinkedin,
+	faAngular,
+	faYoutube
+} from '@fortawesome/free-brands-svg-icons';
+import { socialMediaAccounts } from '../../data/contact-info'; 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+	socialMedias = socialMediaAccounts;
+	instagram = faInstagram;
+	twitter = faTwitter;
+	github = faGithub;
+	facebook = faFacebook;
+	linkedin = faLinkedin;
+	youtube = faYoutube;
+	angular  =faAngular;
+	dev = faDev;
+	socialMediaIcons = [ 
+		{
+			name: "facebook",
+			icon: this.facebook
+		},
+		{
+			name: "twitter",
+			icon: this.twitter
+		},
+		{
+			name: "github",
+			icon: this.github
+		},
+		{
+			name: "linkedin",
+			icon: this.linkedin
+		},
+		{
+			name: "youtube",
+			icon: this.youtube
+		},
+		{
+			name: "dev",
+			icon: this.dev
+		} 
+	]
+	testIcon = this.socialMediaIcons[0].icon;
+	constructor() {
+		console.log(this.socialMediaIcons[0].icon)
+	}
 
-  constructor() { }
+	ngOnInit(): void {
+	}
 
-  ngOnInit(): void {
-  }
+	getIcon(socialMediaName: any) {
+		let iconValue = this.socialMediaIcons.filter( data => data.name == socialMediaName )
+		if (iconValue === undefined || iconValue.length <= 0) {
+			return this.angular
+		}
+		return iconValue[0].icon;
+	}
 
 }
---
+++ frontend/src/app/data/contact-info.ts
@@ -0,0 +1,27 @@
+export let socialMediaAccounts = [
+    {
+        name: "facebook",
+        link: "https://www.facebook.com/jmaclan11",
+    },
+    {
+        name: "twitter",
+        link: "https://twitter.com/AclanJosh",
+    },
+    {
+        name: "github",
+        link: "https://github.com/joshuiaclan",
+    },
+    {
+        name: "dev",
+        link: "https://dev.to/darkknight"
+    },
+    {
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/joshua-aclan-4a367a158/",
+    },
+    {
+        name: "youtube",
+        link: "https://www.youtube.com/channel/UC909yd073aioa5Aaqf-r6Sg",
+    }
+    
+]
