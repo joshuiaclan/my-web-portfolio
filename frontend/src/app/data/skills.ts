1ee0fa463ad6cf697b4c770a372d60c74dc71c69
jmaclan <josh.aclan@gmail.com>
2021-01-13 06:53:15 +0800

    Add skills and work-exp data

---
+++ frontend/src/app/data/skills.ts
@@ -0,0 +1,10 @@
+export let whatIDo = "I have been working in the tech industry for 2 years now, building digital solutions under local companies based in the Philippines. Basically, I customize or make modules for frameworks such as Wordpress and Odoo. In my sparetime, I work on my mini projects to build my portfolio, although all of them are still not out publicly. I also accept freelance work from time to time.";
+
+export let programmingLanguages = [
+    {}
+];
+
+
+export let frameworks = [
+    {}
+];
---
+++ frontend/src/app/data/work-exp.ts
@@ -0,0 +1,34 @@
+export let workExperience = [
+    {
+        company: "Creatizens, Inc.",
+        companyLink: "https://creatizens.com/",
+        jobPosition: "Wordpress Developer",
+        jobDesc: "As my first job I worked with the wordpress framework, making themes, and plugins.Indeed it was a short working experience. But it is also was the most memorable work I ever had, this is where I learned most of the skills I have as a web developer. And I thank the coding wizards I met in this company that opened the doors to the world of web development for me.",
+        dateStart: "April 2019",
+        dateEnd: "July 2019",
+    },
+    {
+        company: "Forecasting and Planning Technologies, Inc.",
+        companyLink: "http://fpti.com.ph/",
+        jobPosition: "ASP.Net Developer",
+        jobDesc: "When I lost my first full time job I went through a plethora of interviews, 16 to be exact, before I got this one. I had zero knowledge of the programming language that they were using, but winners dont make excuses. So, I took it as a challenge and learned the basics in one night. It is the company where I met some of the most cool developers/friends I know till today...",
+        dateStart: "August 2019",
+        dateEnd: "June 2020",
+    },
+    {
+        company: "Adventist University of the Philippines",
+        companyLink: "http://web1.aup.edu.ph/",
+        jobPosition: "IT Instructor",
+        jobDesc: "There was once a time in my life where I swore an oath not to teach... Yet here I am teaching part time. I thought it would be bad but helping others and sharing knowledge is somehow fulfilling. Knowing part of your shared knowledge will be used in the future... or not.",
+        dateStart: "August 2019",
+        dateEnd: "Present",
+    },
+    {
+        company: "Taliform, Inc.",
+        companyLink: "https://www.taliform.com/",
+        jobPosition: "Odoo Developer",
+        jobDesc: "Because of the pandemic I quit my other job, and I looked for another. Luckily, taliform found me with the help of a former workmate who now works for them. It is my first exposure to python and the Odoo framework, thankfuly I have the best mentors who are very knowledgable with it. ",
+        dateStart: "September 2020",
+        dateEnd: "Present",
+    }
+]
