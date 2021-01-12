c9059ec72aa7122473cb9cd460f122ac4cfd596f
jmaclan <josh.aclan@gmail.com>
2021-01-13 06:53:32 +0800

    Add work-exp mobile responsiveness

--- frontend/src/app/components/work-experience/work-experience.component.ts
+++ frontend/src/app/components/work-experience/work-experience.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, HostListener } from '@angular/core';
+import { workExperience } from '../../data/work-exp';
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
   styleUrls: ['./work-experience.component.scss']
 })
 export class WorkExperienceComponent implements OnInit {
-
+  workExperience = workExperience;
+  jobsOnView = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('document:scroll')
+  windowOnScroll() {
+      const screenPxValue: number = 2000;
+      if (document.documentElement.scrollTop > screenPxValue) { 
+          this.jobsOnView = true;
+      }
   }
 
 }
