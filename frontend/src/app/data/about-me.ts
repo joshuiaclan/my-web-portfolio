6b80512bc80481a42cc2c3b5d0a8d7057d496148
jmaclan <josh.aclan@gmail.com>
2021-01-13 06:52:30 +0800

    Add about me component mobile responsiveness

--- frontend/src/app/data/about-me.ts
+++ frontend/src/app/data/about-me.ts
@@ -1,7 +1,7 @@
 export let aboutMe = {
         name: "Joshua Aclan",
         birthday: "12-03-1998",
-        intro: "Are you lost? I hope you're not, my name is on the left side of your screen, I am a fullstack developer by day and an ultimate frisbee player by sunday. I do web programming as good as I throw discs, if you dont know who good I am just believe me.... I am. If you're looking to hire me, heres a short intro... I am a goal driven person, who is bubbly and very optimistic. I can adapt easily to my surroundings, multi-task, and work under pressure while smiling. A fast learner and enthusiastic, I am mentally and physically fit to perform my job. If thats not enough feel free to click that contact button you see on the top right of the screen.",
+        intro: "Welcome to my website! I am a fullstack web developer by day and an ultimate frisbee player by sunday. I do web programming as good as I throw discs. If you're looking forward to work with me, here's what you need to know; I am a goal driven person, who is bubbly and very optimistic. I can easily adapt, multi-task, and work under pressure while smiling. A fast learner and always enthusiastic, I am mentally and physically fit to perform my job. If thats not enough, feel free to click that red button you see on your screen. You need to find it though.",
         jobPosition: "Fullstack Web Developer",
     }
 ;
