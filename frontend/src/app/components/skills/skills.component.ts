54195bd8685ad5cad59f8f28db19b829d07ce008
jmaclan <josh.aclan@gmail.com>
2021-01-13 06:54:05 +0800

    Make skills-component mobile responsive

--- frontend/src/app/components/skills/skills.component.ts
+++ frontend/src/app/components/skills/skills.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { whatIDo } from '../../data/skills';
+import {
+    faHtml5,
+    faCss3,
+    faSass,
+    faBootstrap,
+    faPython,
+    faJsSquare,
+    faNpm,
+    faNode,
+    faAngular,
+    faReact,
+    faPhp,
+    faWordpress,
+    faJoomla,
+    faGit,
+    faGithub,
+    faGitlab,
+    faLinux,
+    faWindows,
+    faApple
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+    selector: 'app-skills',
+    templateUrl: './skills.component.html',
+    styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+    iconSize = '"5x"';
+    whatIDo = whatIDo;
+    techStacks = [
+        faHtml5,
+        faCss3,
+        faSass,
+        faBootstrap,
+        faPython,
+        faJsSquare,
+        faNpm,
+        faNode,
+        faAngular,
+        faReact,
+        faPhp,
+        faWordpress,
+        faJoomla,
+        faGit,
+        faGithub,
+        faGitlab,
+        faLinux,
+        faWindows,
+        faApple
+    ];
+    progLangOnView = false;
+    whatIDoOnView = false;
+    constructor() {
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+    
+    @HostListener('document:scroll')
+    windowOnScroll() {
+        const screenPxValue: number = 770;
+        if (document.documentElement.scrollTop > screenPxValue) { 
+            this.whatIDoOnView = true;
+        }
+        if (document.documentElement.scrollTop > screenPxValue + 150) { 
+            this.progLangOnView = true;
+        }
+    }
 
 }
